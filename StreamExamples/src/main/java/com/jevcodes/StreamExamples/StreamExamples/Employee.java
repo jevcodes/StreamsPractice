052a3cc156a973510c575ab6ef590a5df12abf9e
Jevic J <jevic.jardeleza>
2019-06-03 03:12:07 -0400

    working on initial map examples

---
+++ StreamExamples/src/main/java/com/jevcodes/StreamExamples/StreamExamples/Employee.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.jevcodes.StreamExamples.StreamExamples;
+
+/**
+ * @author Jevic
+ *
+ */
+public class Employee {
+	private String name;
+	private int salary;
+	
+	public Employee(String n, int s) {
+		this.name = n;
+		this.salary = s;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+}
---
+++ StreamExamples/src/main/java/com/jevcodes/StreamExamples/StreamExamples/StreamMap.java
@@ -0,0 +1,31 @@
+package com.jevcodes.StreamExamples.StreamExamples;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMap {
+
+	public static void main(String[] args) {
+
+
+		//initialize employees
+		
+		
+		
+		List<Integer> multiplesOfFive=( Arrays.stream(new Integer[]  
+				{1,2,3,4,5}).map(x->(x*5)))
+				//.forEach(x->System.out.println(x));	
+				.collect(Collectors.toList());
+		for (Integer i : multiplesOfFive)
+			System.out.println(i);
+	
+
+	    Stream<String> capslock = Stream.of("Jevic","Mike","Vince","Caris","Brook");
+	    capslock.map(x->x.toUpperCase()).forEach(x->System.out.println(x));
+	}
+
+}

fb74221e00676b3fd55d389fa92b77c886bc999b
Jevic J <jevic.jardeleza>
2019-06-04 00:20:32 -0400

    Tests with the filter functionality of stream

--- StreamExamples/src/main/java/com/jevcodes/StreamExamples/StreamExamples/StreamMap.java
+++ StreamExamples/src/main/java/com/jevcodes/StreamExamples/StreamExamples/StreamMap.java
@@ -30,7 +30,12 @@ public class StreamMap {
 	    capslock.map(String::toUpperCase).forEach(System.out::println);
 	    
 	    
+	    Stream.Builder<Employee> employeesStream = Stream.builder();
+	    employeesStream.accept(new Employee("Michael Scott", 30000000));
+	    employeesStream.accept(new Employee("Jim Halpert", 60000));
+	    employeesStream.accept(new Employee("Dwight Shrute", 90000));
 	    
+	    employeesStream.build().filter(x -> x.getSalary()>88000).forEach(x->System.out.println(x.getName() + " earn more than 88,000"));
 	    
 	}
 
